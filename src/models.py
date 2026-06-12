9a9e0591e65364f34f28194a4fc86e7a4759cf63
Rafael Mata <133695535+rafataxxx@users.noreply.github.com>
2026-06-12 16:03:25 +0000

    Modelo de datos de instagram

--- src/models.py
+++ src/models.py
@@ -1,19 +1,65 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+import os
+import sys
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+Base = declarative_base()
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False, unique=True)
+    firstname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(100), nullable=False, unique=True)
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Usamos composite primary key porque esta tabla intermedia no tiene un "ID" propio en tu diagrama
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+# Creamos un Enum para los tipos de media permitidos en Instagram
+
+
+class MediaType(enum.Enum):
+    IMAGE = "image"
+    VIDEO = "video"
+    REEL = "reel"
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MediaType))
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+# 5. Tabla Comment
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+
+try:
+    result = render_er(Base, 'diagram.png')
+    print("¡Éxito! Revisa el archivo diagram.png en la raíz de tu proyecto")
+except Exception as e:
+    print("Hubo un problema generando el diagrama")
+    raise e
